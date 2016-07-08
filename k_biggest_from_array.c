b25a5c0339d8556ac81266a7fb81ef353564f8bf
Fernando Correa <fernandocorrea@gmail.com>
2016-07-08 17:21:22 -0300

    Fix k_biggest_from_array.c

--- k_biggest_from_array.c
+++ k_biggest_from_array.c
@@ -5,13 +5,15 @@ int get_left(int node)      { return (node * 2) + 1; }
 int get_right(int node)     { return (node * 2) + 2; }
 
 void dump(int heap[], int size) {
+	/*
 	for(int i = 0; i < size; i++)
 		fprintf(stderr, "%d ", heap[i]);
 	fprintf(stderr, "\n");
+	*/
 }
 
 void swap(int heap[], int to, int from) {
-	fprintf(stderr, "%d[%d] <=> %d[%d]\n", to, heap[to], from, heap[from]);
+	//fprintf(stderr, "%d[%d] <=> %d[%d]\n", to, heap[to], from, heap[from]);
 	int tmp     = heap[to];
 	heap[to]    = heap[from];
 	heap[from]  = tmp;
@@ -27,7 +29,7 @@ void up(int heap[], int node) {
 }
 
 void down(int heap[], int size, int node) {
-	fprintf(stderr, "down(heap, %d, %d)\n", size, node);
+	//fprintf(stderr, "down(heap, %d, %d)\n", size, node);
 	int left = get_left(node);
 	if(left >= size) return;
 	int right = get_right(node);
@@ -47,13 +49,18 @@ void down(int heap[], int size, int node) {
 }
 
 void array_to_heap(int heap[], int size) {
+	/*
 	for(int i = size - 1; i >= 0; i--) {
 		up(heap, i);
+	}
+	*/
+	for(int i = get_parent(size); i >= 0; i--) {
+		down(heap, size, i);
 	}
 }
 
 int pop(int heap[], int *size) {
-	fprintf(stderr, "pop(heap, %d)\n", *size);
+	//fprintf(stderr, "pop(heap, %d)\n", *size);
 	int head = heap[0];
 	swap(heap, 0, --(*size));
 	down(heap, *size, 0);
@@ -65,8 +72,8 @@ void print_k_biggest(int heap[], int size, int k) {
 	array_to_heap(heap, size);
 	dump(heap, size);
 	for(int i = 0; i < k; i++) {
-		fprintf(stderr, "[%d]\n", pop(heap, &size));
-		fprintf(stderr, " ===> "); dump(heap, size);
+		printf("-> %d\n", pop(heap, &size));
+		//fprintf(stderr, " ===> "); dump(heap, size);
 	}
 }
 
